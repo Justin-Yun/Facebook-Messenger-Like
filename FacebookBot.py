7d3d766c6231f1f68c841aaa7dcc52bf15ab8edb
Justin-Yun <36687861+Justin-Yun@users.noreply.github.com>
2020-02-29 18:40:15 -0700

    Add files via upload

---
+++ FacebookBot.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from time import sleep
+from fb_secrets import username, password, link
+
+
+class MessageBot():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def login(self):
+        self.driver.get(link)
+        sleep(5)
+
+        email_in = self.driver.find_element_by_xpath('//*[@id="email"]')
+        email_in.send_keys(username)
+        password_in = self.driver.find_element_by_xpath('//*[@id="pass"]')
+        password_in.send_keys(password)
+
+        login_btn = self.driver.find_element_by_xpath('//*[@id="loginbutton"]')
+        sleep(0.5)
+        login_btn.click()
+
+    def like(self):
+        like = self.driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/span/div[2]/div[2]/div[2]/div[2]/a')
+        like.click()
+        
+    def auto_like(self):
+        while True:
+            sleep(5)
+            # sleep(0.1)
+            self.like()
+
+
+bot = MessageBot()
+bot.login()
+bot.auto_like()
+
+
+
+
+
+
+
